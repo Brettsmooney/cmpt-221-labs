d4073e52504d8407eb4a51da3d4366c648151f4e
Brett <brett.mooney1@marist.edu>
2019-09-22 22:04:20 -0400

    Finished 2 functions of lab part b. Added an interface menu

--- lab01MooneypartbINHERITANCE.py
+++ lab01MooneypartbINHERITANCE.py
@@ -1,4 +1,6 @@
 #Brett Mooney
+import time
+
 class Vehicle:
     def __init__(self, modelID, FCC, FCM, mileage, MPG):
         self.modelIdentification = modelID
@@ -44,6 +46,38 @@ class Vehicle:
             print("Not enough fuel to complete the trip")
             print(" ")
 
+    def refillTank(self):
+        if self.fuelCapCurr != self.fuelCapMax:
+            self.fuelCapCurr = self.fuelCapMax
+            print("Tank has been filled!")
+            print(" ")
+        else:
+            print("Tank is already full!")
+            print(" ")
+
+    #You can also use a float value: time.sleep(5) Delays for 5 seconds.
+    def doADonut(self):
+        if (self.fuelCapCurr != 0):
+            self.fuelCapCurr -= 5.0
+            time.sleep(1)
+            print("*...rrrRRRRrrrr...*")
+            time.sleep(1)
+            print("*...rrrRRRRrrrr...*")
+            time.sleep(.5)
+            print("*...rrrRRRRRRRRSCREEEEEEEEEEEEEEEEEEEEEEEEEE...*")
+            time.sleep(.5)
+            print("*...EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE...*")
+            time.sleep(1)
+            print("that was fun!")
+            time.sleep(1.5)
+            print("")
+        else :
+            time.sleep(1)
+            print("...")
+            time.sleep(1)
+            print("...")
+            print("Not enough fuel, Knievel, shoulda went with the Mercedes.")
+            print("")
 
 
 class CommercialVehicle(Vehicle):
@@ -64,17 +98,49 @@ class UtilityVehicle(Vehicle):
     def setVehicleType(self, vtype):
         self.vehicleType = vtype
 
-testVehicle = Vehicle("chewy000367", 200, 200, 50000, 10)
+print("QA engineers keep out")
+print("")
+programRunning = 1
+testVehicle = Vehicle("walmart001777", 200, 200, 50000, 10)
 testVehicle.printVehicleInfo()
+print("")
+
+while programRunning == 1:
+    print("Select an action by entering the number.")
+    print("1.) -drive")
+    print("2.) -refill tank")
+    print("3.) -donuts")
+    print("4.) -exit program")
+    actionSelection = input()
+
+    if actionSelection == "1":
+        print("How many miles to drive?")
+        milesToDrive = input()
+        try:
+            float(milesToDrive)
+            testVehicle.drive(float(milesToDrive))
+            testVehicle.printVehicleInfo()
+        except:
+            print("Invalid Input")
+    elif actionSelection == "2":
+        testVehicle.refillTank()
+        testVehicle.printVehicleInfo()
+    elif actionSelection == "3":
+        testVehicle.doADonut()
+        testVehicle.printVehicleInfo()
+    elif actionSelection == "4":
+        print("Program will shutdown.")
+        programRunning = 0
+    else :
+        print("Invalid input.")
+
+    print(" ")#spacing
 
 
-print("How many miles to drive? ")
-milesToDrive = input()
-
-testVehicle.drive(int(milesToDrive))
-testVehicle.printVehicleInfo()
 
 
+
+#.  3   5
 
 #x = Vehicle("fedex0001", 100, 200)
 #x.printVehicleInfo()
