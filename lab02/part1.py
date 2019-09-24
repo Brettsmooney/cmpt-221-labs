a393fc57fac08948788acfa07adc762693222b16
Brett <brett.mooney1@marist.edu>
2019-09-23 22:57:12 -0400

    Fixed the routing issue by changing the page referencing from #pagename to /pagename

--- lab02/part1.py
+++ lab02/part1.py
@@ -6,26 +6,36 @@ from flask import render_template
 
 app = Flask(__name__)
 
-#@app.route("/")
-#def hello():
-#    return "<br><hr><center><h1>HOME PAGE</h1></center><hr>"
-
 #static route
 @app.route("/")
 @app.route("/index")
 def index():
-    return "<body><br><hr><center><h1>INDEX PAGE</h1></center><hr><br><center><a href='/about'>about</a></center></body>"
+    return render_template('index.html')
 
 @app.route("/about")
 def hello():
-    return "<br><hr><center><h1>ABOUT PAGE</h1></center><hr><br><center><a href='/index'>index</a></center><hr>"
-
-#HTML_TEMPLATE = Template("<br><hr><center><h1> $egg , does this work? </h1></center><hr><br><center><a href='/about'>about</a></center><hr><br><center><a href='/index'>index</a></center><hr>")
+    return render_template('about.html')
 
 #dynamic route
 @app.route("/name/<variable>")
 def dynamic(variable=None):
     return render_template('dynamic.html', variable=variable)
+
+#@app.route("/")
+#def hello():
+#    return "<br><hr><center><h1>HOME PAGE</h1></center><hr>"
+
+#static route
+#@app.route("/")
+#@app.route("/index")
+#def index():
+#    return "<body><br><hr><center><h1>INDEX PAGE</h1></center><hr><br><center><a href='/about'>about</a></center></body>"
+
+#@app.route("/about")
+#def hello():
+#    return "<br><hr><center><h1>ABOUT PAGE</h1></center><hr><br><center><a href='/index'>index</a></center><hr>"
+
+#HTML_TEMPLATE = Template("<br><hr><center><h1> $egg , does this work? </h1></center><hr><br><center><a href='/about'>about</a></center><hr><br><center><a href='/index'>index</a></center><hr>")
 
 #dynamic route
 #@app.route("/name/<variable>")
